65caa111cbc9ef795210027fd5990e3270a80875
jassyio <joejassyio@gmail.com>
2024-05-22 10:31:44 +0300

    responsive across devices

--- src/App.js
+++ src/App.js
@@ -161,74 +161,73 @@ function App() {
     }
   };
 
-  return (
-    <div className="App">
-      <div className='dashboard'>
-        <img src="./pocket-weather-app-high-resolution-logo-white-transparent (2).png" alt="App Logo" className="app-logo" />
-        <div className="settings">
-          <img src="./settings.png" alt="Settings" onClick={openNotificationSettings} />
-        </div>
-      </div>
-      <div className="search">
-      <div className="search-bar">
-  <img src="./search.png" alt="Search Icon" className="search-icon" />
-  <Input
-    value={location}
-    onChange={(event) => setLocation(event.target.value)}
-    onKeyPress={searchLocation}
-  />
-  <div className="voice-search" onClick={startVoiceRecognition}>
-    <img src="./microphone.png" alt="Microphone Icon" className="microphone-icon" />
-    {isRecording && <div className="recording-indicator">Recording...</div>}
-  </div>
-</div>
-
-      </div>
-      <div className="container">
-        <div className="top">
-          <div className="location-name">
-            <h2>{locationName}</h2>
-          </div>
-          <div className="temp">
-            <h1>{Math.round((selectedDay ? selectedDay.main.temp : data.main?.temp) - 273.15)} °C</h1>
-          </div>
-          <div className="description-container">
-            <p className="description">
-              {selectedDay ? selectedDay.weather[0].description : data.weather?.[0]?.description}
-            </p>
-            <div className="weather-icon">
-              {selectedDay ? renderWeatherIcon(selectedDay.weather[0].main) : renderWeatherIcon(data.weather?.[0]?.main)}
-            </div>
-          </div>
-        </div>
-        <div className="bottom">
-          <div className="feels">
-            <p>temperature</p>
-            <p className="bold">{Math.round((selectedDay ? selectedDay.main.feels_like : data.main?.feels_like) - 273.15)} °C</p>
-          </div>
-          <div className="humid">
-            <p>Humidity</p>
-            <p className="bold">{selectedDay ? selectedDay.main.humidity : data.main?.humidity} %</p>
-          </div>
-          <div className="wind">
-            <p>Wind </p>
-            <p className="bold">{Math.round((selectedDay ? selectedDay.wind.speed : data.wind?.speed) * 3.6)} Km/h</p>
-          </div>
-        </div>
-      </div>
-      <div className="weekly-forecast">
-        {weeklyData.length > 0 ? weeklyData.map((day, index) => (
-          <div
-            key={index}
-            className={`day ${selectedDay === day ? 'selected' : ''}`}
-            onClick={() => handleDayClick(day)}
-          >
-            {new Date(day.dt * 1000).toLocaleDateString('en-US', { weekday: 'short' })}
-          </div>
-        )) : <p>Loading...</p>}
+return (
+  <div className="App">
+    <div className='dashboard'>
+      <img src="./pocket-weather-app-high-resolution-logo-white-transparent (2).png" alt="App Logo" className="app-logo" />
+      <div className="settings">
+        <img src="./settings.png" alt="Settings" onClick={openNotificationSettings} />
       </div>
     </div>
-  );
+    <div className="search">
+      <div className="search-bar">
+        <img src="./search.png" alt="Search Icon" className="search-icon" />
+        <Input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+        />
+        <div className="voice-search" onClick={startVoiceRecognition}>
+          <img src="./microphone.png" alt="Microphone Icon" className="microphone-icon" />
+          {isRecording && <div className="recording-indicator">Recording...</div>}
+        </div>
+      </div>
+    </div>
+    <div className="container">
+      <div className="top">
+        <div className="location-name">
+          <h2>{locationName}</h2>
+        </div>
+        <div className="temp">
+          <h1>{Math.round((selectedDay ? selectedDay.main.temp : data.main?.temp) - 273.15)} °C</h1>
+        </div>
+        <div className="description-container">
+          <p className="description">
+            {selectedDay ? selectedDay.weather[0].description : data.weather?.[0]?.description}
+          </p>
+          <div className="weather-icon">
+            {selectedDay ? renderWeatherIcon(selectedDay.weather[0].main) : renderWeatherIcon(data.weather?.[0]?.main)}
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          <p>temperature</p>
+          <p className="bold">{Math.round((selectedDay ? selectedDay.main.feels_like : data.main?.feels_like) - 273.15)} °C</p>
+        </div>
+        <div className="humid">
+          <p>Humidity</p>
+          <p className="bold">{selectedDay ? selectedDay.main.humidity : data.main?.humidity} %</p>
+        </div>
+        <div className="wind">
+          <p>Wind </p>
+          <p className="bold">{Math.round((selectedDay ? selectedDay.wind.speed : data.wind?.speed) * 3.6)} Km/h</p>
+        </div>
+      </div>
+    </div>
+    <div className="weekly-forecast">
+      {weeklyData.length > 0 ? weeklyData.map((day, index) => (
+        <div
+          key={index}
+          className={`day ${selectedDay === day ? 'selected' : ''}`}
+          onClick={() => handleDayClick(day)}
+        >
+          {new Date(day.dt * 1000).toLocaleDateString('en-US', { weekday: 'short' })}
+        </div>
+      )) : <p>Loading...</p>}
+    </div>
+  </div>
+);
 }
 
 export default App;
